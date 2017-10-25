4e4fcc0d1f8051924f2aeaeb7376593c10556130
zhoujunpeng <zjp0432@163.com>
2017-10-25 17:56:27 +0800

    normalize install feature and opration tips

--- index.js
+++ index.js
@@ -1,77 +1,61 @@
-'use strict';
+const Fs = require('fs');
+const Path = require('path');
+const Shell = require('shelljs');
+const BoiUtils = require('boi-utils');
 
-require('shelljs/global');
-
-const _ = require('lodash');
-const Ora = require('ora');
-const Chalk = require('chalk');
-const Promise = require("bluebird");
-
-const DEFAULT_OPTIONS = {
-  // 需要安装的模块
-  modules: [],
-  // 是否自动检查package.json收集的依赖包安装情况
-  autoCheck: true
-};
-
-// npm淘宝镜像
+// cnpm registry
 const NPM_REGISTRY = 'https://registry.npm.taobao.org';
 
-module.exports = function (options) {
-  // 检测cnpm是否可用
-  const CNPM_AVALIABLE = !!(exec('cnpm -v',{silent:true}).stdout);
+/**
+ * @module boi/aux-autoinstall
+ * @param {boolean} isInstall whether need install modules automatically
+ * @param {Array} modules modules that need to be installed
+ * @return {Promise}
+ */
+module.exports = function (isInstall, modules) {
+  // install when isInstall is true or .boirc file not exists
+  const NeedInstallDeps = isInstall || !Fs.existsSync(Path.join(process.cwd(), '.boirc'));
+  if (!NeedInstallDeps) {
+    // return empty promise if not need install
+    return new Promise(resolve => {
+      resolve();
+    });
+  }
 
-  let spinner = Ora(Chalk.cyan.bold('Checking dependencies...')).start();;
+  // check if cnpm is available
+  const CNPM_AVALIABLE = !!(Shell.exec('cnpm -v', {
+    silent: true
+  }).stdout);
 
-  let _options = options || Object.assign({}, DEFAULT_OPTIONS);
-  Promise.try(() => {
-    let _modulesNeedChecked = [];
-    // 储存指定的modules
-    if (_options.modules && _options.modules.length !== 0) {
-      _modulesNeedChecked = _modulesNeedChecked.concat(_options.modules);
-    }
-    return _modulesNeedChecked;
-  }).then((modules) => {
-    let _modulesNeedInstalled = [];
-    // 检查依赖包本地是否已安装
-    if (modules && modules.length !== 0) {
-      modules.forEach(function (module) {
-        try {
-          require.resolve(module.split('@')[0]);
-        } catch (e) {
-          _modulesNeedInstalled.push(module);
+  return BoiUtils.log.loading(new Promise((resolve, reject) => {
+    try {
+      if (modules && modules.length !== 0) {
+        if (CNPM_AVALIABLE) {
+          Shell.exec(`cnpm install ${modules.join(' ')} --save-dev`, {
+            silent: true
+          }, () => {
+            Fs.writeFileSync(Path.join(process.cwd(), '.boirc'), JSON.stringify({
+              dependencies: modules
+            }));
+            resolve({
+              msg: 'Install dependencies successfully'
+            });
+          });
+        } else {
+          Shell.exec(`npm install ${modules.join(' ')} --save-dev --registry=${NPM_REGISTRY}`, {
+            silent: true
+          }, () => {
+            Fs.writeFileSync(Path.join(process.cwd(), '.boirc'), JSON.stringify({
+              dependencies: modules
+            }));
+            resolve({
+              msg: 'Install dependencies successfully'
+            });
+          });
         }
-      });
-    }
-    return _modulesNeedInstalled;
-  }).then((modules) => {
-    // autoCheck为true时执行一次package.json依赖包检测
-    if (_options.autoCheck) {
-      if (CNPM_AVALIABLE) {
-        exec('cnpm install', {
-          silent: true
-        });
-      } else {
-        exec('npm install --registry=' + NPM_REGISTRY, {
-          silent: true
-        });
       }
+    } catch (e) {
+      reject(e || 'Install dependencies fails');
     }
-    // 安装package.json未收集的依赖包
-    if (modules && modules.length !== 0) {
-      if (CNPM_AVALIABLE) {
-        exec(`cnpm install ${modules.join(' ')} --save-dev`, {
-          silent: true
-        });
-      } else {
-        exec(`npm install ${modules.join(' ')} --save-dev --registry=${NPM_REGISTRY}`, {
-          silent: true
-        });
-      }
-    }
-    spinner.succeed(Chalk.cyan.bold('All dependencies have been installed\n'));
-  }).catch((err) => {
-    spinner.fail(Chalk.red(err.stack));
-    process.exit(1);
-  });
+  }), 'Installing dependencies...');
 };
