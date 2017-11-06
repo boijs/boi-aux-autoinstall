66518a5b4694358f6a5f3e327e963d5934b5186b
zhoujunpeng <zjp0432@163.com>
2017-11-06 14:38:25 +0800

    executed asynchronously and return a Promise Object

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # boi-aux-autoinstall
-a tool used to install dependencies for [boi](http://boijs.github.io ) project
+[![npm](https://img.shields.io/npm/l/express.svg)](https://github.com/boijs/boi-aux-autoinstall)
+[![npm version](https://badge.fury.io/js/boi-aux-autoinstall.svg)](https://badge.fury.io/js/boi-aux-autoinstall)
+
+install dependencies for [Boi](https://github.com/boijs/boi) project
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Fs = require('fs');
 const Path = require('path');
 const Shell = require('shelljs');
+const Utils = require('util');
 const BoiUtils = require('boi-utils');
 
 // cnpm registry
@@ -15,6 +16,7 @@ const NPM_REGISTRY = 'https://registry.npm.taobao.org';
 module.exports = function (isInstall, modules) {
   // install when isInstall is true or .boirc file not exists
   const NeedInstallDeps = isInstall || !Fs.existsSync(Path.join(process.cwd(), '.boirc'));
+
   if (!NeedInstallDeps) {
     // return empty promise if not need install
     return new Promise(resolve => {
@@ -22,37 +24,19 @@ module.exports = function (isInstall, modules) {
     });
   }
 
-  // check if cnpm is available
-  const CNPM_AVALIABLE = !!(Shell.exec('cnpm -v', {
-    silent: true
-  }).stdout);
-
   return BoiUtils.log.loading(new Promise((resolve, reject) => {
     try {
       if (modules && modules.length !== 0) {
-        if (CNPM_AVALIABLE) {
-          Shell.exec(`cnpm install ${modules.join(' ')} --save-dev`, {
-            silent: true
-          }, () => {
-            Fs.writeFileSync(Path.join(process.cwd(), '.boirc'), JSON.stringify({
-              dependencies: modules
-            }));
-            resolve({
-              msg: 'Install dependencies successfully'
-            });
+        Shell.exec(`npm install ${modules.join(' ')} --save-dev --registry=${NPM_REGISTRY}`, {
+          silent: true
+        }, () => {
+          Fs.writeFileSync(Path.join(process.cwd(), '.boirc'), Utils.inspect({
+            dependencies: modules
+          }));
+          resolve({
+            msg: 'Install dependencies succeed'
           });
-        } else {
-          Shell.exec(`npm install ${modules.join(' ')} --save-dev --registry=${NPM_REGISTRY}`, {
-            silent: true
-          }, () => {
-            Fs.writeFileSync(Path.join(process.cwd(), '.boirc'), JSON.stringify({
-              dependencies: modules
-            }));
-            resolve({
-              msg: 'Install dependencies successfully'
-            });
-          });
-        }
+        });
       }
     } catch (e) {
       reject(e || 'Install dependencies fails');
